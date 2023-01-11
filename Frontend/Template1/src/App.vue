a8a88c0bad87aa4e4a8d4de8f02d1eb873c56fba
Petter Axcell <petteraxcell03@gmail.com>
2023-01-11 13:38:36 +0100

    Nav and Title did it

--- Frontend/Template1/src/App.vue
+++ Frontend/Template1/src/App.vue
@@ -1,19 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Navegador from './components/Navegador.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1> BAR PACO </h1>
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <Navegador />
     </div>
   </header>
 
@@ -21,65 +16,14 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header{
+  display: grid;
+  grid-template-columns: 1;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
+h1{
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  padding: 10px;
+  margin: 0;
 }
 </style>
---
+++ Frontend/Template1/src/components/Navegador.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+    data() {
+        return {
+            Nav: ['INFORMACIÓN','TAPAS','ENTRANTES','PRIMEROS','SEGUNDOS','BOCATAS FRIOS','BOCATAS CALIENTES','POSTRES','BEBIDAS'],
+        }
+    },
+}
+
+</script>
+
+
+
+
+<template>
+  <nav>
+    <ul>
+        <li v-for="item in Nav" :key="item">
+            <router-link :to="item" class="ElementNav">{{ item }}</router-link>
+        </li>
+    </ul>
+    </nav>
+</template>
+
+
+
+
+
+
+
+<style scoped>
+nav{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+
+nav ul {
+    display:flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    border-top: dashed 2px #cdc9c3;
+    border-bottom: dashed 2px #cdc9c3;
+}
+
+nav ul li {
+    padding: 10px;
+    list-style:none;
+}
+
+.ElementNav {
+    padding: 2px;
+    padding-left: 20px;
+    padding-right: 20px;
+    text-decoration: none;
+    color: #373a40;
+}
+
+.ElementNav:visited  { text-decoration: none; color:#373a40; }
+.ElementNav:hover { text-decoration: none; color:#968c83; }
+</style>
